0675d542c42e7db81e346c76d4137ad211db1414
Mahesh <85110955+m9hesh@users.noreply.github.com>
2023-02-05 11:07:10 +0530

    Create StackUsingStackAndArrays.c

---
+++ StackUsingStackAndArrays.c
@@ -0,0 +1,79 @@
+//Stack implementation using Stack and Arrays
+#include<stdio.h>
+#include<conio.h>
+#define SIZE 10;
+
+struct stack{
+    int list[top];
+    int top;
+}sp;
+//PUSH method
+void push{
+  if(sp.top==SIZE-1)
+    printf("\n Stack is empty UNDERFLOW");
+  else
+    sp.list[++sp.top]=x;
+  return;
+}
+//POP method
+void pop(){
+  if(sp.top==-1)
+    printf("Stack is Empty");
+  else
+    printf("\n Poped Element is %d", sp.list[sp.top--]);
+  return;
+}
+//PEEK method
+void peek(){
+  if(sp.top==-1)
+    printf("\n STACK is Empty, i.e. UNDERFLOW");
+  else
+    printf("\n toppest element %d", sp.list[sp.top]);
+  return;
+}
+//SHOW method
+ void show(){
+   int i;
+   printf("\n Stack elements are \n");
+   if(sp.top==-1)
+     printf("\n STACK is Empty");
+   else
+     for(int i=sp.top;i>=0;i--)
+       printf("%d\n", sp.list[i]);
+   return;
+ }
+
+//main method
+int main(){
+  int x, opt=1;
+  sp.top=-1;
+  
+  while(opt!=5){
+    printf("\n 1.PUSH \n 2.POP\n 3.PEEK\n 4.SHOW \n\n Enter Your Choice: \n");
+    scanf("%d",&opt);
+    
+    switch(opt){
+      case 1:
+        printf("\n Enter The No:");
+        scanf("%d",&x);
+        push(x);
+        break;
+      case 2:
+        pop();
+        break;
+      case 3:
+        peek();
+        break;
+      case 4:
+        show();
+        break;
+        default:
+        printf("\n Please Enter a Valid Choice");
+      }
+   }
+  return 0;
+}
+
+     
+  
+    
